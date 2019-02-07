eb6a06723c8ae2968e33868492cecac9ef223c94
Leonardo Gomes <lgdevlop@gmail.com>
2019-02-07 21:11:52 -0200

    Toolchain para o desenvolvimento do WebApp

---
+++ src/client/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/style.css'
+import './index.html'
+
+ReactDOM.render(<h1>React Rules!</h1>, document.getElementById('app'))
